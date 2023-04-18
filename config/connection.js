faadce67c9a25c05a0b879fd309e80857630af37
paullsshin <paullsshin@hotmail.com>
2023-04-18 15:53:53 -0700

    changed some files working on getting the setup working

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/studentsDB');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/lets-get-to-networking');
 
 
 module.exports = mongoose.connection;
